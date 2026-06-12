c7733f2aa3217b2b0eb665f9d9f0eadd0528e43f
xn7c <262377966+xn7c@users.noreply.github.com>
2026-06-11 22:35:35 -0400

    Update tarball

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/ffa10e26ae11d676b2db836259889f1f571cb14f.tar.gz") {} }:
+{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/8c91a71d13451abc40eb9dae8910f972f979852f.tar.gz") {} }:
 
 pkgs.mkShell {
   packages = with pkgs; [
